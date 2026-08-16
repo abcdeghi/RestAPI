55c87b7ec50c8f6cea13caec5d2b6a1e3482537c
abcdeghi <semin4599@naver.com>
2026-08-16 13:25:04 +0900

    Initial commit

---
+++ src/main/java/com/back/p67restapi/P67RestapiApplication.java
@@ -0,0 +1,13 @@
+package com.back.p67restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P67RestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P67RestapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/back/p67restapi/P67RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.p67restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P67RestapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
